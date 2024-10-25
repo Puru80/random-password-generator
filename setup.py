7c70408b37acc3fcc85f511c54f682badb6a5e66
Puru80 <puru.agar99@gmail.com>
2024-10-25 16:46:07 +0530

    Updated setup.py file

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
     requirements = fh.read()
 
 setup(
-    name="mytool",
+    name="passgen",
     version="0.0.1",
-    author="Puru Agarwa",
+    author="Puru Agarwal",
     author_email="puru.agar99@gmail.com",
     license="GPL-3.0-or-later",
     description="<short description for the tool>",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Puru80/mytool",
-    py_modules=["my_tool", "app"],
+    py_modules=["passgen", "app"],
     packages=find_packages(),
     install_requires=[requirements],
     python_requires=">=3.7",
@@ -26,6 +26,6 @@ setup(
     ],
     entry_points="""
         [console_scripts]
-        cooltool=my_tool:cli
+        cooltool=passgen:cli
     """,
 )
