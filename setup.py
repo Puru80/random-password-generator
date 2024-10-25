a117c490535daece87676efa1737292fc4a02fac
Puru80 <puru.agar99@gmail.com>
2024-10-25 16:35:08 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read()
+
+setup(
+    name="mytool",
+    version="0.0.1",
+    author="Puru Agarwa",
+    author_email="puru.agar99@gmail.com",
+    license="GPL-3.0-or-later",
+    description="<short description for the tool>",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Puru80/mytool",
+    py_modules=["my_tool", "app"],
+    packages=find_packages(),
+    install_requires=[requirements],
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ],
+    entry_points="""
+        [console_scripts]
+        cooltool=my_tool:cli
+    """,
+)
